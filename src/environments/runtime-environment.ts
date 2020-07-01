909586d8a1686011a5b4ec35f64edd6f9bb16939
Moritz Mahler <moritzmahler45@gmail.com>
2020-07-01 12:07:33 +0200

    changed serviceInstanceId and cusomEndpoint

--- src/environments/runtime-environment.ts
+++ src/environments/runtime-environment.ts
@@ -10,7 +10,7 @@ if (!injectedEnv) {
     'DASHBOARD DEFAULT DEVELOPMENT ENV ACTIVE - DID YOU FORGET TO INJECT A RUNTIME ENV?'
   );
   seedEnv = {
-    serviceInstanceId: '73047820-8d36-4323-bf2b-77dc887a707e',
+    serviceInstanceId: /*'73047820-8d36-4323-bf2b-77dc887a707e',*/ '4474507e-efba-470b-8386-31be2c442568',
     token: 'Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vdWFhLnN5c3RlbS5jZi5ob2IubG9jYWwvdG9rZW5fa2V5cyIsImtpZCI6ImtleS0xIiwidHlwIjoiSldUIn0.eyJqdGkiOiJmNjU3MWExMzE1ZmY0YzAzYTg5YmZhZjNjMjYyYTg3MyIsInN1YiI6ImFjNTcxOGQ4LThlZjEtNDc1ZS1hZWYzLTUyNjk5YzlmNjAwZSIsInNjb3BlIjpbImNsb3VkX2NvbnRyb2xsZXJfc2VydmljZV9wZXJtaXNzaW9ucy5yZWFkIiwib3BlbmlkIl0sImNsaWVudF9pZCI6Im9zYi1sb2ctbWV0cmljLXRlc3Quc3lzdGVtLmNmLmhvYi5sb2NhbCIsImNpZCI6Im9zYi1sb2ctbWV0cmljLXRlc3Quc3lzdGVtLmNmLmhvYi5sb2NhbCIsImF6cCI6Im9zYi1sb2ctbWV0cmljLXRlc3Quc3lzdGVtLmNmLmhvYi5sb2NhbCIsImdyYW50X3R5cGUiOiJhdXRob3JpemF0aW9uX2NvZGUiLCJ1c2VyX2lkIjoiYWM1NzE4ZDgtOGVmMS00NzVlLWFlZjMtNTI2OTljOWY2MDBlIiwib3JpZ2luIjoiRXZvaWxhU1NPIiwidXNlcl9uYW1lIjoibW1haGxlciIsImVtYWlsIjoibW1haGxlckBldm9pbGEuZGUiLCJhdXRoX3RpbWUiOjE1OTM1NTEwMzIsInJldl9zaWciOiI0ZDc3MzdhZiIsImlhdCI6MTU5MzU1MTAzMiwiZXhwIjoxNTkzNTk0MjMyLCJpc3MiOiJodHRwczovL3VhYS5zeXN0ZW0uY2YuaG9iLmxvY2FsL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbIm9wZW5pZCIsIm9zYi1sb2ctbWV0cmljLXRlc3Quc3lzdGVtLmNmLmhvYi5sb2NhbCIsImNsb3VkX2NvbnRyb2xsZXJfc2VydmljZV9wZXJtaXNzaW9ucyJdfQ.L50Ie-IEkzC-F4fVo-5Zfpu-pvqsP6pZ4Frp_1ENJF2LKiRNe9qcxnLfNwxiib8nyBq-ZOUc1EJHot3tamHr0sF-Dnhk9lwaV48Ezzv0kl7n3g_KyRhbPKuA3BWN528tnxOpTVKT23X5T85VYCdDNB49QKiBPAK34Uf9O-qK8w6dOyPr713K6vNfpFY9M0kbXKTFjZ93MdaZKIb6cixMJ_NWQGRFWZke8NTxtXOwDNC20RoiG0OF50yaWJHcAyweUU2mf3SxwEeiQMAcHeaAd9iNaTCfJ4zNSWLzMxistZYE0OZllRbqdYhhBmKulPF3DlDejEgBCpsNogKZ6Lhdrg',
 
     baseUrls: {
@@ -18,8 +18,8 @@ if (!injectedEnv) {
     },
     production: false,
     customEndpoints: [
-      //{ url: 'https://osb-log-metric-dashboard-backend-test.system.cf.hob.local', identifier: 'log-metric-backend' },
-      { url: 'https://log-metric-backend-feature.system.cf.hob.local', identifier: 'log-metric-backend' },
+      { url: 'https://osb-log-metric-dashboard-backend-test.system.cf.hob.local', identifier: 'log-metric-backend' },
+      //{ url: 'https://log-metric-backend-feature.system.cf.hob.local', identifier: 'log-metric-backend' },
       //{ url: 'http://localhost:8080', identifier: 'log-metric-backend' },
       { url: 'http://localhost:8081', identifier: 'osb-backup-manager' }
     ],
