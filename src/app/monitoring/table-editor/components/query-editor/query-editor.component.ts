dc41ce3e1c8603b9237d51a619cf7225536ba853
Moritz Mahler <moritzmahler45@gmail.com>
2020-07-06 17:59:55 +0200

    created basic query editor with text area to fill in must part of es-bool-query

--- src/app/monitoring/table-editor/components/query-editor/query-editor.component.ts
+++ src/app/monitoring/table-editor/components/query-editor/query-editor.component.ts
@@ -21,6 +21,14 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ESQuery } from '../../model/es-query';
 import { RawQuery } from '../../model/raw-query';
+import { SearchService } from 'app/monitoring/shared/services/search.service';
+import { Observable } from 'rxjs';
+import { Field } from 'app/monitoring/aggregation-editor/model/field';
+import { Store } from '@ngrx/store';
+import { getQueriesState, RunQuery } from '../../store';
+import { filter, take } from 'rxjs/operators';
+import { environment } from 'environments/runtime-environment';
+import { ServiceBinding } from 'app/monitoring/model/service-binding';
 
 @Component({
   selector: 'sb-query-editor',
@@ -35,20 +43,68 @@ export class QueryEditorComponent implements OnInit {
   @Output('save-new-query')
   save_new_query = new EventEmitter();
 
+
+  scope: ServiceBinding | null = null;
   //raw query + name & id
   query: ESQuery;
   // not null after successfull query-editor-query-test
   valid_query: RawQuery;
+  validating = false;
+  valid = false;
+  query_test_result_hint = "";
+  es_bool_query_text_area_must: string = "{'match' : { '_index' : '*-logmessages'} }";
 
+  public fields$: Observable<Map<string, Array<Field>>>;
+  
+  //fetched_indexes: Array<string>;
 
-  constructor() { }
+  constructor(private readonly searchService: SearchService,
+    private es_query_store: Store<ESQuery>) { }
 
   ngOnInit() {
 
-        var must_val = [{'match' : { '_index' : '*-logmessages'} }] 
-        var raw_query = new RawQuery([], [], [], must_val)
-        this.query = new ESQuery('123', 'nice query', raw_query)
+    this.fields$ = this.searchService.getMappingWithType();
 
+  }
+
+
+
+  test_query(){
+    console.log('testing query');
+
+    var must_val = [this.es_bool_query_text_area_must] 
+    var raw_query = new RawQuery([], [], [], must_val)
+    this.query = new ESQuery('test query', raw_query)
+    this.query_test_result_hint = "";
+    this.es_query_store.dispatch(new RunQuery(this.query, this.scope!));
+    this.es_query_store.select(getQueriesState).pipe(filter(k => !k.queries.running)).pipe(take(1)).subscribe(k => {
+      var esbq_run_result = k.queries.run_result;
+      this.validating = false;
+      if (esbq_run_result != null){
+        this.valid = true;
+        this.query_test_result_hint = "empty result";
+        // check number of hits -- '+' operator converting string to number
+        const hit_count = +esbq_run_result.responses[0].hits.total; 
+        //console.log('hits: ' + this.hit_count);
+        if (hit_count > 0){
+          this.query_test_result_hint = "query valid";
+          
+        
+        }
+      }
+      else{
+        this.valid = false;
+        this.query_test_result_hint = "query not valid";
+      }
+      //console.log(esbq_run_result);
+    })
+
+  }
+
+
+  // user did select an app binding via drop down
+  did_select_scope(service_binding: ServiceBinding){
+    this.scope = service_binding;
   }
 
 
--- src/environments/runtime-environment.ts
+++ src/environments/runtime-environment.ts
@@ -11,7 +11,7 @@ if (!injectedEnv) {
   );
   seedEnv = {
     serviceInstanceId: '73047820-8d36-4323-bf2b-77dc887a707e',
-    token: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJQOG5VNGpDdDM0UXhqenExdXNhMXItZjVXb2VWeWp3SXo1RTZLQUxzN2FFIn0',
+    token: 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJQOG5VNGpDdDM0UXhqenExdXNhMXItZjVXb2VWeWp3SXo1RTZLQUxzN2FFIn0.eyJleHAiOjE1OTQwODI2MjgsImlhdCI6MTU5NDA0NjYyOCwianRpIjoiODg2N2Q5MTktZGYwOC00YTEyLThlOGQtOTUzOTNlMWE4YWJiIiwiaXNzIjoiaHR0cHM6Ly9zc28uc3lzdGVtLmNmLmhvYi5sb2NhbC9hdXRoL3JlYWxtcy9UaW1BRyIsInN1YiI6Ijg3MjM1ZTE2LTU0N2QtNDY1NS1iYTk1LTlmYjhmMmRhODdhMyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxvZy1tZXRyaWMtZGFzaGJvYXJkIiwic2Vzc2lvbl9zdGF0ZSI6IjRkZmI1MWExLTMwY2ItNDhlNi05OTBjLTkwOTYyOTYyODRhNCIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsidGltLXVzZXIiLCJ0aW0tYWRtaW4iXX0sInNjb3BlIjoidGltYWciLCJyb2xlcyI6WyJ0aW0tdXNlciIsInRpbS1hZG1pbiJdLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJqaGV5bCIsImVtYWlsIjoiamhleWxAZXZvaWxhLmRlIn0.eimYj-HeE5NrYaBmGAjligBumt7RB8WSLDJmyghHAWLoGeaEvaDHJIeuRkL40fbL2G0StZYO1BjRCEpUgsdVv95fpj4QbyuTdYT6RWafGCTJg_c-6V0nocz7DbeRV_jvA9YrgBCR0M2HIiN5TVpgNY6ytuZuujIHi9ZF_j1jj9uxhht8I-LLGqlPsxybiMK9rjZZo4vOf8JDDbdpEjKeniEH37kfu92oRBG6tFpM5N7zEYUKcN6J7YSEUJyzHolGbYsEz_CXEHpee_is9JK0XWcNWKuhYfBbZHjnwkDXScatedMuXY1vcaEMoP-qirOYS3Yx_ypHBv3wV8IEecThrA',
 
     baseUrls: {
       serviceBrokerUrl: 'https://osb-log-metric-test.system.cf.hob.local'
@@ -21,7 +21,7 @@ if (!injectedEnv) {
       //{ url: 'https://osb-log-metric-dashboard-backend-test.system.cf.hob.local', identifier: 'log-metric-backend' },
       //{ url: 'https://log-metric-backend-feature.system.cf.hob.local', identifier: 'log-metric-backend' },
       //{ url: 'http://localhost:8081', identifier: 'log-metric-backend' },
-      { url: 'https://osb-log-metric-backend-keycloak-test.system.cf.hob.local', identifier: 'log-metric-backend' },
+      { url: 'https://osb-log-metric-dashboard-backend-keycloak-test.system.cf.hob.local', identifier: 'log-metric-backend' },
       { url: 'http://localhost:8081', identifier: 'osb-backup-manager' }
     ],
     ui: {
