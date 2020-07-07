fa0c5171738ed0e364feff491d0538e89a2d650f
Jannik Heyl <Jannik.Heyl@googlemail.com>
2020-07-07 16:44:22 +0200

    fix: wrong endpoint in dev config

--- src/environments/runtime-environment.ts
+++ src/environments/runtime-environment.ts
@@ -11,7 +11,7 @@ if (!injectedEnv) {
   );
   seedEnv = {
     serviceInstanceId: '73047820-8d36-4323-bf2b-77dc887a707e',
-    token: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJQOG5VNGpDdDM0UXhqenExdXNhMXItZjVXb2VWeWp3SXo1RTZLQUxzN2FFIn0',
+    token: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJQOG5VNGpDdDM0UXhqenExdXNhMXItZjVXb2VWeWp3SXo1RTZLQUxzN2FFIn0.eyJleHAiOjE1OTQwODYxNDEsImlhdCI6MTU5NDA1MDE0MSwianRpIjoiNTg1YTQ0NDktYzAxZS00ZmFiLTkxZjYtYTg1ZTUxYTc3OWMwIiwiaXNzIjoiaHR0cHM6Ly9zc28uc3lzdGVtLmNmLmhvYi5sb2NhbC9hdXRoL3JlYWxtcy9UaW1BRyIsInN1YiI6Ijg3MjM1ZTE2LTU0N2QtNDY1NS1iYTk1LTlmYjhmMmRhODdhMyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImxvZy1tZXRyaWMtZGFzaGJvYXJkIiwic2Vzc2lvbl9zdGF0ZSI6IjIyMjNiNDllLWI4NGQtNGVhZS04MDg4LWFkMjc2NDQ1MjI4MCIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsidGltLXVzZXIiLCJ0aW0tYWRtaW4iXX0sInNjb3BlIjoidGltYWciLCJyb2xlcyI6WyJ0aW0tdXNlciIsInRpbS1hZG1pbiJdLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJqaGV5bCIsImVtYWlsIjoiamhleWxAZXZvaWxhLmRlIn0.drrOPwTgpwE1YP-9JuId-dXR5wxc3uT5-D86HfL5nYgN2iWr5UpVxAuFxtpzETnAaUEOqoxDoP_Vw_uml9wTEvpFuYzg7ejlxgjge7CPxuJ4RnFxPW5zEbZQqJzFhkbjAfCPTgJ9iGVAfIuHswYCnRjOSl5-CKZNT8c0pCtQnYn5N19gizTmIsMxyykf2WA3D8dN1AU-cB8_LCW_9PNgBoT-VitzqwDlSuA4rKJwNQ3cJUG-Vs9VCDX7uGi0RDLM24fmDLxwBencHpb1z2bcgD82-GqGcsMcJNlB00pENXLooYEFtEQ1f1LKOOEYXcRfR2tL2oQK1FsCbH4uJxv08Q',
 
     baseUrls: {
       serviceBrokerUrl: 'https://osb-log-metric-test.system.cf.hob.local'
@@ -21,7 +21,7 @@ if (!injectedEnv) {
       //{ url: 'https://osb-log-metric-dashboard-backend-test.system.cf.hob.local', identifier: 'log-metric-backend' },
       //{ url: 'https://log-metric-backend-feature.system.cf.hob.local', identifier: 'log-metric-backend' },
       //{ url: 'http://localhost:8081', identifier: 'log-metric-backend' },
-      { url: 'https://osb-log-metric-backend-keycloak-test.system.cf.hob.local', identifier: 'log-metric-backend' },
+      { url: 'https://osb-log-metric-dashboard-backend-keycloak-test.system.cf.hob.local', identifier: 'log-metric-backend' },
       { url: 'http://localhost:8081', identifier: 'osb-backup-manager' }
     ],
     ui: {
